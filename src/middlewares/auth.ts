3ffaf72cbddf56110c6c928b0c6a22a3f6c5bcc9
yangseungbin <tanja1206@naver.com>
2020-09-29 22:05:07 +0900

    [ADD] token check middleware

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -7,7 +7,6 @@ export const authMiddleware = (
   next: NextFunction
 ) => {
   const token: any = req.headers["authorization"].split("Bearer ")[1];
-  console.log(req.headers);
   if (!token) return res.status(403).json({ message: "로그인 되어있지 않음" });
   jwt.verify(token, req.app.get("jwt-secret"), (err, decoded) => {
     if (err) return res.status(403).json({ message: "토큰 만료됨" });
