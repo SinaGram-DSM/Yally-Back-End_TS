cafa42a3e4fd025a638ed0859cda7d4e97e27bff
yangseungbin <tanja1206@naver.com>
2020-09-30 22:50:11 +0900

    [ADD] get timeline

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import post from "./post";
+import timeline from "./timeline";
 
 const router = Router();
 
 router.use("/post", post);
+router.use("/timeline", timeline);
 
 export default router;
