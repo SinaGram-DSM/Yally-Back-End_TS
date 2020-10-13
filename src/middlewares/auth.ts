900e35d7d2181bcc6c9c50f1315fcca43ad9925f
yangseungbin <tanja1206@naver.com>
2020-10-13 13:23:44 +0900

    [MODIFY] return res

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -7,8 +7,8 @@ export const authMiddleware = (
   next: NextFunction
 ) => {
   const token: any = req.headers["authorization"];
-  const bearer = token.split("Bearer ")[1];
   if (!token) return res.status(403).json({ message: "로그인 되어있지 않음" });
+  const bearer = token.split("Bearer ")[1];
   jwt.verify(bearer, req.app.get("jwt-secret"), (err, decoded) => {
     if (err) return res.status(403).json({ message: "토큰 만료됨" });
     req["decoded"] = decoded;
